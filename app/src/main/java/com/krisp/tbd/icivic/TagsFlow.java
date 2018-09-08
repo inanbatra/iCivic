1b7f95236fc3a77977f56e8666c999c15751f18f
iecurius <saahilyechuri@gmail.com>
2018-09-08 18:13:08 -0400

    Merge branch 'master' of https://github.com/iecurius/icivic

--- app/src/main/java/com/krisp/tbd/icivic/TagsFlow.java
+++ app/src/main/java/com/krisp/tbd/icivic/TagsFlow.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
