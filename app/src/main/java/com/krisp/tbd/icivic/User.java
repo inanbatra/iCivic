a99c3f49f1edeaa339ec9be4a8c8e2ebb93661af
iecurius <saahilyechuri@gmail.com>
2018-09-08 11:51:33 -0400

    Created User object frame
    ++ User.java

---
+++ app/src/main/java/com/krisp/tbd/icivic/User.java
@@ -0,0 +1,33 @@
+package com.krisp.tbd.icivic;
+
+public class User {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public User (String name, phoneNumber, emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
