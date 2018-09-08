cf2df5be34f7dfa9c9c24ae49724c2631b0cd12a
Prashikh Agrawal <prashikh.agrawal1@gmail.com>
2018-09-08 16:48:25 -0400

    made a new page/activity

---
+++ app/src/main/java/com/krisp/tbd/icivic/myviewActivity.java
@@ -0,0 +1,13 @@
+package com.krisp.tbd.icivic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myview);
+    }
+}
