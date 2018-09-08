dd86e4e829cc349ed421918ce8ed1ca664badbe0
Prashikh Agrawal <prashikh.agrawal1@gmail.com>
2018-09-08 17:30:43 -0400

    adding picture taking functionality

--- app/src/main/java/com/krisp/tbd/icivic/myviewActivity.java
+++ app/src/main/java/com/krisp/tbd/icivic/myviewActivity.java
@@ -1,13 +1,27 @@
 package com.krisp.tbd.icivic;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class myviewActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myview);
+
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager() != null)) {
+            startActivityForResult(takeP);
+        }
     }
 }
