2b76696c3c3091a4288572e47debbf16fe6a5485
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2018-09-08 23:16:06 -0400

    everything is good

--- app/src/main/java/com/krisp/tbd/icivic/MyHome.java
+++ app/src/main/java/com/krisp/tbd/icivic/MyHome.java
@@ -1,6 +1,5 @@
 package com.krisp.tbd.icivic;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
--- app/src/main/java/com/krisp/tbd/icivic/TagsFlow.java
+++ app/src/main/java/com/krisp/tbd/icivic/TagsFlow.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import android.content.Intent;
 
 public class TagsFlow extends AppCompatActivity implements View.OnClickListener{
-    Button fork0;
+    //  Button fork0;
     Button fork1;
     Button fork2;
 
@@ -18,28 +18,26 @@ public class TagsFlow extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tags_flow);
-        fork0 = (Button) findViewById(R.id.parks);
+        //  fork0 = (Button) findViewById(R.id.parks);
         fork1 = (Button) findViewById(R.id.city);
         fork2 = (Button) findViewById(R.id.home);
 
         txtView = (TextView) findViewById(R.id.alert);
 
-        fork0.setOnClickListener(this);
+        //  fork0.setOnClickListener(this);
         fork1.setOnClickListener(this);
         fork2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) { //when user clicks on fork0 or fork1, this method executed
-        if(v.getId() == R.id.parks) {
-            //code to execute
-            txtView.setText("Park!");
+//        if(v.getId() == R.id.parks) {
+//            Intent myIntent = new Intent(TagsFlow.this, OurParks.class);
+//            TagsFlow.this.startActivity(myIntent);
 
-
-        }
         if(v.getId() == R.id.city) {
-            //code to execute
-            txtView.setText("not park:(");
+            Intent myIntent = new Intent(TagsFlow.this, OurCity.class);
+            TagsFlow.this.startActivity(myIntent);
         }
 
         if(v.getId() == R.id.home) {
