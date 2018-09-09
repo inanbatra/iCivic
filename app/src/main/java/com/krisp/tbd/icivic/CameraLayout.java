318b8e96d1324caef861ffc60ec918b7b6a1c2ce
kaushik <36049613+kmanchikanti@users.noreply.github.com>
2018-09-08 22:11:27 -0400

    Camera

---
+++ app/src/main/java/com/krisp/tbd/icivic/CameraLayout.java
@@ -0,0 +1,34 @@
+package com.krisp.tbd.icivic;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+
+
+public class CameraLayout extends AppCompatActivity {
+private static final int CAMERA_REQUEST = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cameralayout);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+           // public void onClick(View view) {
+             //   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+               //         .setAction("Action", null).show();
+        }
+
+        public void camera(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+    }
+
+}
