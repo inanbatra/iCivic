471c110a7d65204a93eb26609b2c7b9d5df99708
Inan Batra <inanbatra@gmail.com>
2018-09-08 23:49:14 -0400

    q2w3

--- app/src/main/java/com/krisp/tbd/icivic/WelcomeActivity.java
+++ app/src/main/java/com/krisp/tbd/icivic/WelcomeActivity.java
@@ -24,7 +24,7 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
-        //skip button temporary
+        //skip button temporary//dfhirfh
         Button skip = (Button)findViewById(R.id.skip);
 
         skip.setOnClickListener(new View.OnClickListener() {
